ab56d5f44aad00628d12df6098914febfe12c454
Vincent Langat <kimutailangat033@gmail.com>
2022-09-19 17:28:48 +0300

    create a portfolio container file

---
+++ src/PortfolioContainer/PortfolioContainer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import {TOTAL_SCREENS} from "../utilities/commonUtils"
+
+function PortfolioContainer() {
+
+    const mapALlScreens = ()=>{
+        return (
+            TOTAL_SCREENS.map((screen)=>(
+                (screen.component) ? <screen.component screenName={screen.screen_name}key={screen.screen_name}
+                id={screen.screen_name} /> : <div key={screen.screen_name}></div>
+            )
+
+            )
+        )
+    }
+
+  return (
+    <div className='portfolio-container'>
+        {mapALlScreens()}
+
+    </div>
+  )
+}
+
+export default PortfolioContainer
