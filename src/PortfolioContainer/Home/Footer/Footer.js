8e19de22bd0447187784250adc46c57f9e31c253
Vincent Langat <kimutailangat033@gmail.com>
2022-09-19 17:09:05 +0300

    Create a Footer folder containing footer.js and footer.css

---
+++ src/PortfolioContainer/Home/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+        <div className='footer-parent'>
+            <img src={require ("../../../assets/Home/shape-bg.png")} alt = "shape bg" />
+
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
