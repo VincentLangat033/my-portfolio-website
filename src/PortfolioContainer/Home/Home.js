e4b8e1c04d49893a851a578192bf5356fdb13081
Vincent Langat <kimutailangat033@gmail.com>
2022-09-19 17:15:18 +0300

    create Home file

---
+++ src/PortfolioContainer/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./home.css"
+import Profile from './Profile/Profile'
+import Footer from './Footer/Footer'
+
+function Home() {
+  return (
+    <div className='home-container'>
+        <Profile />
+        <Footer />
+
+
+
+    </div>
+  )
+}
+
+export default Home
