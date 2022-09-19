3efbc83413874d45f674dd79177e19a7a0e74cc5
Vincent Langat <kimutailangat033@gmail.com>
2022-09-19 12:37:03 +0300

    Add cv to root folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Profile from './PortfolioContainer/Home/Profile';
+
+function App() {
+  return (
+    <div className="App">
+     <Profile />
+    </div>
+  );
+}
+
+export default App;
