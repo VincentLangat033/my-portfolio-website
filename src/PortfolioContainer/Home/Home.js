7ddf6c2f0a4e9a6da203893666dbfc8a2ba47f2e
Vincent Langat <kimutailangat033@gmail.com>
2022-09-20 10:48:00 +0300

    render the Header component

--- src/PortfolioContainer/Home/Home.js
+++ src/PortfolioContainer/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./home.css"
+import Header from './Header/Header'
 import Profile from './Profile/Profile'
 import Footer from './Footer/Footer'
 
 function Home() {
   return (
     <div className='home-container'>
+        <Header />
         <Profile />
         <Footer />
 
