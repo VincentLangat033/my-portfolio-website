be494e4ff4c1d4a360d60ffff05fb6ab76dab650
Vincent Langat <kimutailangat033@gmail.com>
2022-09-22 19:08:00 +0300

    set up axios and toastify for back end functionality

--- src/PortfolioContainer/ContactMe/ContactMe.js
+++ src/PortfolioContainer/ContactMe/ContactMe.js
@@ -1,5 +1,7 @@
 import React,{useEffect, useState} from 'react'
 import Typical from 'react-typical'
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
 import imgBack from '../../../src/images/mailz.jpeg'
 import load1 from '../../../src/images/load2.gif'
@@ -7,6 +9,7 @@ import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
 import ScrollService from '../../utilities/ScrollService';
 import Animations from '../../utilities/Animations';
 import './contactme.css'
+
 
 function ContactMe(props) {
     let fadeInScreenHandler = (screen) =>{
@@ -37,6 +40,35 @@ function ContactMe(props) {
     console.log(message)
     console.log(name)
     console.log(email)
+
+    const submitForm = async(e) =>{
+        e.preventDefault();
+        try {
+            let data ={
+                name, email, message
+            };
+            setBool(true)
+            const res =  await axios.post(`/contact`, data);
+            if(name.length === 0 || email.length === 0 || message.length === 0){
+                setBanner(res.data.msg)
+                toast.error(res.data.msg)
+                setBool(false)
+            }else if(res.status === 200){
+                setBanner(res.data.msg)
+                toast.success(res.data.msg)
+                setBool(false)
+            }
+
+        }
+            
+       catch (error) {
+            console.log(error)
+            
+        }
+    } 
+       
+
+
 
 
 
@@ -98,7 +130,7 @@ function ContactMe(props) {
                     <h4>Send your EMail Here</h4>
                     <img src={imgBack} alt=" not found" />
                 </div>
-                <form>
+                <form onSubmit={submitForm}>
                     <p> {banner}</p>
                     <label htmlFor='name'>Name</label>
                     <input type="text" 
@@ -120,6 +152,7 @@ function ContactMe(props) {
                     <div className='send-btn'>
                         <button type='submit'>
                             send <i className='fa fa-paper-plane' />
+                            {bool ? (<b className='load'> <img src={load1} alt="oops"/></b> ): ("")}
 
                         </button>
                     </div>
