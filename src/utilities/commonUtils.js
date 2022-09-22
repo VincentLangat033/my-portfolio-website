22a3b8a513e04a90fe32fcbcbc5710bb47d6ae1a
Vincent Langat <kimutailangat033@gmail.com>
2022-09-22 14:05:46 +0300

    add contact me component to common utils

--- src/utilities/commonUtils.js
+++ src/utilities/commonUtils.js
@@ -1,6 +1,7 @@
 import Home from "../PortfolioContainer/Home/Home"
 import AboutMe from "../PortfolioContainer/AboutMe/AboutMe"
 import Resume from "../PortfolioContainer/Resume/Resume"
+import ContactMe from "../PortfolioContainer/ContactMe/ContactMe"
 
 export const TOTAL_SCREENS = [
     {
@@ -14,6 +15,10 @@ export const TOTAL_SCREENS = [
     {
         screen_name: "Resume",
         component: Resume,
+    },
+    {
+        screen_name: "ContactMe",
+        component: ContactMe,
     }
 ]
 export const GET_SCREEN_INDEX = (screen_name) =>{
